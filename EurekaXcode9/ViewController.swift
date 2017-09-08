e55a6465ef49f42559702e82dcef038011f7dcac
BAN Jun <banjun@gmail.com>
2017-09-08 11:19:02 +0900

    minimum example that Eureka with iOS 11 SDK displays empty header/footer for header/footer-less sections

--- EurekaXcode9/ViewController.swift
+++ EurekaXcode9/ViewController.swift
@@ -1,25 +1,18 @@
-//
-//  ViewController.swift
-//  EurekaXcode9
-//
-//  Created by BAN Jun on 2017/09/08.
-//  Copyright © 2017 banjun. All rights reserved.
-//
-
 import UIKit
+import Eureka
 
-class ViewController: UIViewController {
+class ViewController: FormViewController {
+    init() {
+        super.init(style: .plain)
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        form
+            +++ Section()
+            <<< LabelRow() {$0.title = "Label"}
+            <<< LabelRow() {$0.title = "Label"}
+            +++ Section("Header")
+            <<< LabelRow() {$0.title = "Label"}
+            <<< LabelRow() {$0.title = "Label"}
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    required init?(coder aDecoder: NSCoder) {fatalError()}
 }
-
